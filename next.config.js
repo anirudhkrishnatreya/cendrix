71ed29284f5f598d599e641e0db4eb5e03c18b77
anirudhkrishnatreya <80411739+anirudhkrishnatreya@users.noreply.github.com>
2026-06-03 15:30:12 +0530

    Updates

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
-const explicitBasePath = process.env.NEXT_PUBLIC_BASE_PATH
-const basePath = explicitBasePath ?? (repoName && !repoName.endsWith('.github.io') ? `/${repoName}` : '')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
-  ...(basePath
-    ? {
-        basePath,
-        assetPrefix: basePath,
-      }
-    : {}),
   images: {
     unoptimized: true,
   },
